46b49ef64d83c6d63e1550a08e378a10f8826451
KarynaLohvina <karinalogvina0@gmail.com>
2020-03-16 18:08:09 +0300

    feat: slider

--- script.js
+++ script.js
@@ -85,7 +85,54 @@ CLOSE_BUTTON.addEventListener('click', (event) => {
     document.getElementById('email').value = '';
     document.getElementById('message_block').classList.add('hidden');
 });
+/*Slider*/
 
+let items = document.querySelectorAll('.carusel .slider-item');
+let currentItem = 0;
+let isEnabled = true;
 
+function changeCurrentItem(n) {
+	currentItem = (n + items.length) % items.length;
+}
 
+function hideItem(direction) {
+	isEnabled = false;
+	items[currentItem].classList.add(direction);
+	items[currentItem].addEventListener('animationend', function() {
+		this.classList.remove('active', direction);
+	});
+}
+
+function showItem(direction) {
+	items[currentItem].classList.add('next', direction);
+	items[currentItem].addEventListener('animationend', function() {
+		this.classList.remove('next', direction);
+		this.classList.add('active');
+		isEnabled = true;
+	});
+}
+
+function nextItem(n) {
+	hideItem('to-left');
+	changeCurrentItem(n + 1);
+	showItem('from-right');
+}
+
+function previousItem(n) {
+	hideItem('to-right');
+	changeCurrentItem(n - 1);
+	showItem('from-left');
+}
+
+document.querySelector('.control.left').addEventListener('click', function() {
+	if (isEnabled) {
+		previousItem(currentItem);
+	}
+});
+
+document.querySelector('.control.right').addEventListener('click', function() {
+	if (isEnabled) {
+		nextItem(currentItem);
+	}
+});
 });
